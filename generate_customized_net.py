ff6450ff9c8dc91c5a52fe8882fc158fdeeb5972
Steven <steven.ou.black@outlook.com>
2016-02-19 11:11:43 +0800

    Custom model path

--- generate_customized_net.py
+++ generate_customized_net.py
@@ -7,30 +7,31 @@ import argparse
 def parseArgs():
     myparser = argparse.ArgumentParser(description='generate models of specified class(include _background)')
     myparser.add_argument('--cls',type=int)
-
+    myparser.add_argument('--out','-o')
     return myparser.parse_args()
-
 
 
 if __name__ == '__main__':
     args = parseArgs()
     ncls = args.cls
+    outdir = args.out
     nbox = ncls * 4
-    cmt_tag = {"##ncls":ncls,"##nbboxpred":nbox}
-    if os.path.exists('models'):
-        print 'models already exists, Please backup it.'
+    cmt_tag = {"##ncls":ncls,"##nbboxpred":nbox,"##outdir":outdir}
+#    if os.path.exists('models'):
+    if os.path.exists(outdir):
+        print 'Outdir already exists, Please backup it.'
         exit(1)
     subdir_vgg16 = 'VGG16/faster_rcnn_alt_opt'
     subdir_vgg_m = 'VGG_CNN_M_1024/faster_rcnn_alt_opt'
     src = [subdir_vgg16, subdir_vgg_m]
-    os.makedirs('models/' + subdir_vgg16)  
-    os.makedirs('models/' + subdir_vgg_m)
+    os.makedirs(os.path.join(outdir, subdir_vgg16))  
+    os.makedirs(os.path.join(outdir, subdir_vgg_m))
 
     for i in src:
         files = os.listdir(os.path.join('models-bak',i))
         for j in files:
             src_path = os.path.join('models-bak',i,j)
-            dst_path = os.path.join('models',i,j)
+            dst_path = os.path.join(outdir,i,j)
             fin = open(src_path)
             fout = open(dst_path,'w')
             lines = fin.readlines()
